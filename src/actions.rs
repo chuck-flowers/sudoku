63ed83affd69183d29864571c58511aa76ecb2cd
Timothy Flowers <t_chuck_flowers@yahoo.com>
2019-09-22 11:01:06 -0400

    Removes uneccessary abstractions from the sudoku actions

--- src/actions.rs
+++ src/actions.rs
@@ -1,89 +1,64 @@
 use crate::grid::coordinate::Coordinate;
 use crate::grid::Grid;
 use crate::grid::SudokuValue;
-use std::fmt::Debug;
 
 /// An action that has already been applied
-pub struct AppliedAction<A: SudokuAction> {
-    action: A,
+pub struct AppliedAction {
+    coordinate: Coordinate,
+    value: SudokuValue,
 }
 
-impl<A: SudokuAction> AppliedAction<A> {
+impl AppliedAction {
+    pub fn coordinate(&self) -> &Coordinate {
+        &self.coordinate
+    }
+    
     /// Undoes the action on the grid
-    pub fn revert(self, grid: &mut Grid) -> UnappliedAction<A> {
-        let action = self.action;
-        action.revert(grid);
-
-        UnappliedAction { action }
+    pub fn revert(self, grid: &mut Grid) -> UnappliedAction {
+        grid[&self.coordinate] = None;
+        UnappliedAction {
+            coordinate: self.coordinate,
+            value: self.value,
+        }
     }
 }
 
 /// An action that has not been applied yet
 #[derive(Debug)]
-pub struct UnappliedAction<A: SudokuAction> {
-    action: A,
-}
-
-impl<A: SudokuAction> UnappliedAction<A> {
-    /// Creates a new UnappliedAction
-    pub fn new(action: A) -> Self {
-        UnappliedAction { action }
-    }
-
-    /// Applies the action to a Grid
-    pub fn apply(self, grid: &mut Grid) -> AppliedAction<A> {
-        let action = self.action;
-        action.apply(grid);
-        AppliedAction { action }
-    }
-
-    /// Increments the action
-    pub fn increment(&mut self) -> bool {
-        self.action.increment()
-    }
-
-    pub fn into(self) -> A {
-        self.action
-    }
-}
-
-pub trait SudokuAction: Debug {
-    fn apply(&self, grid: &mut Grid);
-    fn revert(&self, grid: &mut Grid);
-    fn increment(&mut self) -> bool;
-}
-
-#[derive(Debug)]
-pub struct WriteAction {
+pub struct UnappliedAction {
     coordinate: Coordinate,
     value: SudokuValue,
 }
 
-impl Into<Coordinate> for WriteAction {
-    fn into(self) -> Coordinate {
-        self.coordinate
-    }
-}
-
-impl WriteAction {
+impl UnappliedAction {
     pub fn new(coordinate: Coordinate) -> Self {
         Self {
             coordinate,
             value: SudokuValue::one(),
         }
     }
+
+    /// Applies the action to a Grid
+    pub fn apply(self, grid: &mut Grid) -> AppliedAction {
+        grid[&self.coordinate] = Some(self.value);
+        AppliedAction {
+            coordinate: self.coordinate,
+            value: self.value,
+        }
+    }
+
+    pub fn coordinate(&self) -> &Coordinate {
+        &self.coordinate
+    }
+
+    /// Increments the action
+    pub fn increment(&mut self) -> bool {
+        self.value.inc()
+    }
 }
 
-impl SudokuAction for WriteAction {
-    fn apply(&self, grid: &mut Grid) {
-        grid[&self.coordinate] = Some(self.value);
-    }
-
-    fn revert(&self, grid: &mut Grid) {
-        grid[&self.coordinate] = None;
-    }
-
-    fn increment(&mut self) -> bool {
-        self.value.inc()
+impl Into<Coordinate> for UnappliedAction {
+    fn into(self) -> Coordinate {
+        self.coordinate
     }
 }
